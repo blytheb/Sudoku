e182b8f16a6d53e99e7eaf448acd44dcabc4b41f
B <blytheballesteros@lclark.edu>
2020-03-26 18:24:58 -1000

    added textfiles with diff level of hardness

--- sudoku.py
+++ sudoku.py
@@ -42,6 +42,7 @@ def is_valid(bo, row, col, num):
                 return False
     return True
 
+
 # recursive to keep trying to fill empty squares with right numbers
 def solve_sudoku(bo):
     loc = [0, 0]
@@ -59,17 +60,12 @@ def solve_sudoku(bo):
 
 
 def main():
-    matrix = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-              [6, 0, 0, 1, 9, 5, 0, 0, 0],
-              [0, 9, 8, 0, 0, 0, 0, 6, 0],
-              [8, 0, 0, 0, 6, 0, 0, 0, 3],
-              [4, 0, 0, 8, 0, 3, 0, 0, 1],
-              [7, 0, 0, 0, 2, 0, 0, 0, 6],
-              [0, 6, 0, 0, 0, 0, 2, 8, 0],
-              [0, 0, 0, 4, 1, 9, 0, 0, 5],
-              [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+    matrix = [[int(x) for x in line.strip().split(' ')] for line in open('hard9.txt')]
+    print(matrix)
+    print("This is what you start with")
     print_b(matrix)
     if solve_sudoku(matrix):
+        print("This is the final answer")
         print_b(matrix)
     else:
         print("no answer")
